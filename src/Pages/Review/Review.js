88bf0c5ea5886a70257ff8203d4f083a7f12ed57
Kawser Ahmed Shovon <ahmedshovoncse13@gmail.com>
2021-10-19 20:31:35 +0600

    features added

--- src/Pages/Review/Review.js
+++ src/Pages/Review/Review.js
@@ -20,7 +20,7 @@ const Review = () => {
                                 <div className="d-flex">
                                     <img src="https://i.ibb.co/rfRWmLd/developer-1.jpg" alt="" style={{ height: "70px" }} className="rounded-circle me-4" />
                                     <div className="">
-                                        <h3>ইসলাম মাহিম</h3>
+                                        <h4>ইসলাম মাহিম</h4>
                                         <p className="text-secondary mb-0">Patient</p>
                                         <FontAwesomeIcon icon={faStar} className="me-2 text-warning" />
                                         <FontAwesomeIcon icon={faStar} className="me-2 text-warning" />
@@ -31,7 +31,7 @@ const Review = () => {
                                 </div>
                                 <FontAwesomeIcon icon={faQuoteRight} className="me-2 right-quote" style={{ fontSize: "55px", color: "#EEEEEE" }} />
                             </div>
-                            <p className="text-secondary mt-4 px-4">The doctor who treated me was extremely patience and kind to me. I truly appreciate the service I got from the hospital in particular to doctors</p>
+                            <p className="text-secondary mt-2 px-4">The doctor who treated me was extremely patience and kind to me. I truly appreciate the service I got from the hospital in particular to doctors</p>
                         </div>
                     </Col>
                     <Col xs={12} md={6} className="">
@@ -40,7 +40,7 @@ const Review = () => {
                                 <div className="d-flex">
                                     <img src="https://i.ibb.co/7kzyfTL/developer-2.jpg" alt="" style={{ height: "70px" }} className="rounded-circle me-4" />
                                     <div className="">
-                                        <h3>Raiyan Ahmed</h3>
+                                        <h4>Raiyan Ahmed</h4>
                                         <p className="text-secondary mb-0">Patient</p>
                                         <FontAwesomeIcon icon={faStar} className="me-2 text-warning" />
                                         <FontAwesomeIcon icon={faStar} className="me-2 text-warning" />
@@ -51,7 +51,7 @@ const Review = () => {
                                 </div>
                                 <FontAwesomeIcon icon={faQuoteRight} className="me-2 right-quote" style={{ fontSize: "55px", color: "#EEEEEE" }} />
                             </div>
-                            <p className="text-secondary mt-4 px-4">Dr. Castañeda is a confident, caring, informative provider that I have complete faith and trust in.</p>
+                            <p className="text-secondary mt-2 px-4">Dr. Castañeda is a confident, caring, informative provider that I have complete faith and trust in.</p>
                         </div>
                     </Col>
                     <Col xs={12} md={6} className="">
@@ -60,7 +60,7 @@ const Review = () => {
                                 <div className="d-flex">
                                     <img src="https://i.ibb.co/F6mwj7D/developer-6.jpg" alt="" style={{ height: "70px" }} className="rounded-circle me-4" />
                                     <div className="">
-                                        <h3>রাহাত আহমেদ</h3>
+                                        <h4>রাহাত আহমেদ</h4>
                                         <p className="text-secondary mb-0">Patient</p>
                                         <FontAwesomeIcon icon={faStar} className="me-2 text-warning" />
                                         <FontAwesomeIcon icon={faStar} className="me-2 text-warning" />
@@ -71,7 +71,7 @@ const Review = () => {
                                 </div>
                                 <FontAwesomeIcon icon={faQuoteRight} className="me-2 right-quote" style={{ fontSize: "55px", color: "#EEEEEE" }} />
                             </div>
-                            <p className="text-secondary mt-4 px-4">Excellent visit. This was for a second opinion and an attempt to see what help an injection would do. I would have happily had him do the surgery if I lived closer.</p>
+                            <p className="text-secondary mt-2 px-4">Excellent visit. This was for a second opinion and an attempt to see what help an injection would do. I would have happily had him do the surgery if I lived closer.</p>
                         </div>
                     </Col>
                 </Row>
--- src/Pages/Services/ServiceInfo/ServiceInfo.js
+++ src/Pages/Services/ServiceInfo/ServiceInfo.js
@@ -7,7 +7,7 @@ import services from "./serviceData"
 const ServiceInfo = () => {
     const { id } = useParams()
     const service = services.find(item => item.id === parseInt(id))
-    const { img, name, info } = service
+    const { img, name, info, img1, img2, img3 } = service
     return (
         <div className="w-75 mx-auto pb-5 pt-3 service-info">
             <div className="bg-white p-4 pt-1 rounded shadow text-center">
@@ -15,6 +15,17 @@ const ServiceInfo = () => {
                 <div>
                     <h5 className="fw-bold my-2 text-danger border-bottom border-danger border-2 pb-2">{name}</h5>
                     <p className="text-secondary">{info}</p>
+                </div>
+                <div className="row row-cols-1 row-cols-md-3 row-cols-lg-3 g-4">
+                    <div className="col">
+                        <img src={img1} alt="" className="img-fluid detail-img shadow border border-2" />
+                    </div>
+                    <div className="col">
+                        <img src={img2} alt="" className="img-fluid detail-img shadow border border-2" />
+                    </div>
+                    <div className="col">
+                        <img src={img3} alt="" className="img-fluid detail-img shadow border border-2" />
+                    </div>
                 </div>
             </div>
             <ScrollButton />
